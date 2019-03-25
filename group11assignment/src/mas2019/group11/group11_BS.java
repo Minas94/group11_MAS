f981550b54220ed07b866edc46d16cab42101616
Minas94 <marco.miculin@gmail.com>
2019-03-25 17:48:24 +0100

    .

---
+++ group11assignment/src/mas2019/group11/group11_BS.java
@@ -0,0 +1,25 @@
+package mas2019.group11;
+
+import genius.core.bidding.BidDetails;
+import genius.core.boaframework.OfferingStrategy;
+
+public class group11_BS extends OfferingStrategy{
+
+	@Override
+	public BidDetails determineOpeningBid() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BidDetails determineNextBid() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
