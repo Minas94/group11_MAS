4549c45711a279d034784bb7133c3f2ca2592e2d
Minas94 <43180683+Minas94@users.noreply.github.com>
2019-04-02 14:59:11 +0200

    Update group11_AS.java

--- group11assignment/src/mas2019/group11/group11_AS.java
+++ group11assignment/src/mas2019/group11/group11_AS.java
@@ -1,47 +1,102 @@
 package mas2019.group11;
 
 import java.util.List;
+import java.util.Map;
 
 import genius.core.Bid;
+import genius.core.bidding.BidDetails;
 import genius.core.boaframework.AcceptanceStrategy;
 import genius.core.boaframework.Actions;
+import genius.core.boaframework.NegotiationSession;
+import genius.core.boaframework.OfferingStrategy;
+import genius.core.boaframework.OpponentModel;
 import genius.core.uncertainty.UserModel;
+
 
 public class group11_AS extends AcceptanceStrategy {
 
+	private double bestGoal = 0.95;
+	private double ASconst = 0.15;
+	
+	private group11_OM om = new group11_OM();
+
+	int round;
+
+	@Override
+	public void init(NegotiationSession negoSession, OfferingStrategy strat, OpponentModel opponentModel,
+			Map<String, Double> parameters) throws Exception {
+		this.negotiationSession = negoSession;
+		offeringStrategy = strat;
+		
+	}
+
 	@Override
 	public Actions determineAcceptability() {
-		Bid receivedBid = negotiationSession.getOpponentBidHistory().getLastBid();
-		Bid lastOwnBid = negotiationSession.getOwnBidHistory().getLastBid();
-		if (receivedBid == null || lastOwnBid == null) {
-			return Actions.Reject;
-		}
+		round++;
+		
+		//SortedSet<Double> utilityList = new SortedSet<Double>();
+		
+		Bid estimateBid = negotiationSession.getOpponentBidHistory().getLastBid();
+		BidDetails lastOpponentBid = negotiationSession.getOpponentBidHistory().getLastBidDetails();
+		BidDetails lastOwnBid = negotiationSession.getOwnBidHistory().getLastBidDetails();
+		UserModel myUserModel = negotiationSession.getUserModel();
+		
+		
+		
+		
 
-		UserModel userModel = negotiationSession.getUserModel();
-		if (userModel != null) {
-			List<Bid> bidOrder = userModel.getBidRanking().getBidOrder();
-			if (bidOrder.contains(receivedBid)) {
-				double percentile = (bidOrder.size() - bidOrder.indexOf(receivedBid)) / (double) bidOrder.size();
-				if (percentile < 0.1)
+		if (myUserModel == null) {
+			if (lastOpponentBid != null && lastOwnBid != null) {
+
+				// since our BS already take in account a Tit4Tat model there is no reason to
+				// make AS very complex
+				// I think it is enough to accept if it over a certain treshold + if the utility
+				// that we get is > than what we are going to offer from BS to offer
+				/*if(round>155) {
+				double e = lastOwnBid.getMyUndiscountedUtil();
+				utilityList.add(e);
+				utilityList.sort(c);
+				}*/
+				if (lastOpponentBid.getMyUndiscountedUtil() > bestGoal) {
+					System.out.println(round +" we accepted because utility was high");
 					return Actions.Accept;
-			}
-		} else {
-			// we have a normal utilityspace
-			double otherLastUtil = negotiationSession.getUtilitySpace().getUtility(receivedBid);
-			double myLastUtil = negotiationSession.getUtilitySpace().getUtility(lastOwnBid);
-			if (otherLastUtil >= 0.9 * myLastUtil) {
-				return Actions.Accept;
-			}
+				}
+				if (round <= 175 ) {	
+					return Actions.Reject;
+				}
+				if (lastOpponentBid.getMyUndiscountedUtil() >= bestGoal
+						|| lastOpponentBid.getMyUndiscountedUtil() >= offeringStrategy.getNextBid().getMyUndiscountedUtil()) {
+					System.out.println(round);
+					System.out.println(round +"we accepted because utility was higher than our next bid");
+					return Actions.Accept;
+				}
+				if (om.getBidEvaluation(estimateBid) > lastOpponentBid.getMyUndiscountedUtil()+ ASconst ) {
+					return Actions.Reject;
+				}
+
+			
+				
+				//if(utilityList.contains(negotiationSession.getOpponentBidHistory().getLastBidDetails().getMyUndiscountedUtil()
+					//	||negotiationSession.getOpponentBidHistory().getLastBidDetails().getMyUndiscountedUtil() > utilityList.)) {
+				//	return Actions.Accept;
+				//}
+				
+
+			} else
+				return Actions.Reject;
+		}
+		if (myUserModel != null) {
+			// TODO
+			List<Bid> bidRanking = myUserModel.getBidRanking().getBidOrder();
+
 		}
 		return Actions.Reject;
 	}
 
-	
-
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return "Dolos_AS";
 	}
 
 }
